2926700f76523d95394cdf9d86c0d570cf6bff0f
Pavani <pavanibhatia5801@gmail.com>
2022-10-14 12:11:38 +0530

    Updates

---
+++ precache-manifest.a63018e8f6b0d93fbb299b3a0bc99515.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c612d90a311f1635dc52",
+    "url": "/st4Project/static/css/main.ff3ccad8.chunk.css"
+  },
+  {
+    "revision": "c612d90a311f1635dc52",
+    "url": "/st4Project/static/js/main.c612d90a.chunk.js"
+  },
+  {
+    "revision": "d9d48b8a15cba81e6782",
+    "url": "/st4Project/static/css/1.bd7abf07.chunk.css"
+  },
+  {
+    "revision": "d9d48b8a15cba81e6782",
+    "url": "/st4Project/static/js/1.d9d48b8a.chunk.js"
+  },
+  {
+    "revision": "5cc659a35cabb0fd01c0",
+    "url": "/st4Project/static/js/runtime~main.5cc659a3.js"
+  },
+  {
+    "revision": "9e251c4f8066a7cbf9d32136d1974176",
+    "url": "/st4Project/static/media/image5.9e251c4f.svg"
+  },
+  {
+    "revision": "bab88693a251feace0fc7c22aebc516f",
+    "url": "/st4Project/static/media/image2.bab88693.svg"
+  },
+  {
+    "revision": "9a124d34c676691fdd76934034df277d",
+    "url": "/st4Project/static/media/image4.9a124d34.svg"
+  },
+  {
+    "revision": "85f953b431b92b3a3552e4e492549fbc",
+    "url": "/st4Project/index.html"
+  }
+];
